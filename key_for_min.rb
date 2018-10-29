cd45581204447205105c3909a40636c0d28889db
Corina Talavera <corina.talavera@gmail.com>
2018-10-29 23:23:58 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
-def key_for_min_value(name_hash)
+cat_food = {"chicken" => "7", "fish" => "3", "beef" => "4"}
+def key_for_min_value(cat_food)
+  #=> "fish"
 
 end
